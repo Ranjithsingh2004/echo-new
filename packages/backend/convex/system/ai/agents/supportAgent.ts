3730a543c7273330c87e409e49c0833b661565ea
Ranjith Singh <ranjithsingh5424@gmail.com>
2025-11-21 13:59:24 +0530

    22 ai search tool

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -4,10 +4,12 @@ import { components } from "../../../_generated/api";
 import { resolve } from "path";
 import { escalateConversation } from "../tools/escalateConversation";
 import { resolveConversation } from "../tools/resolveConversation";
+import { SUPPORT_AGENT_PROMPT } from "../constants";
+
 
 export const supportAgent = new Agent(components.agent , {
 chat: openai.chat('gpt-4o-mini'),
-instructions: `You are a customer support agent. Use "resolveConversation" tool when user expresses finalization of the conversation. Use "escalateConversation" tool when user expresses frustration, or requests a human explicitly.`,
+instructions: SUPPORT_AGENT_PROMPT,
 tools:{
     resolveConversation,
     escalateConversation,
